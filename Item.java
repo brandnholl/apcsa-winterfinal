2d4fcb7c2babe81e9507097011e531795347da0b
Brandon <brandnlholl@gmail.com>
2023-12-20 21:07:13 -0800

    Main and item class

---
+++ Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    private String itemName;
+    private String itemDescription;
+
+
+    public Item(String itemName, String itemDescription) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+}
